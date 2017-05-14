df5ddc08ff6b5d772e20addcc8c14f1f81287270
Hp_Workplace <farkas0528@gmail.com>
2017-05-14 14:25:27 +0200

    #5

--- src/hu/helixlab/FileOperation.java
+++ src/hu/helixlab/FileOperation.java
@@ -1,6 +1,6 @@
 package hu.helixlab;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * Created by Hp_Workplace on 2017. 05. 13..
@@ -42,5 +42,37 @@ public class FileOperation {
             }
         }
 
+    }
+
+    public boolean isFilesEquals(String path1, String path2) {
+
+        boolean equal = true;
+        try {
+            BufferedReader read1 = new BufferedReader(new FileReader(path1));
+            BufferedReader read2 = new BufferedReader(new FileReader(path2));
+
+            String line1 = read1.readLine();
+            String line2 = read2.readLine();
+
+            while (line1 != null || line2 != null) {
+                if (line1 == null || line2 == null) {
+                    equal = false;
+                    break;
+                } else if (!line1.equalsIgnoreCase(line2)) {
+                    equal = false;
+                    break;
+                }
+                line1 = read1.readLine();
+                line2 = read2.readLine();
+            }
+            read1.close();
+            read2.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return equal;
     }
 }
--- src/hu/helixlab/Main.java
+++ src/hu/helixlab/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         FileOperation fileOperation = new FileOperation();
-        fileOperation.printDirsInDirectory("C:\\AMD");
-        fileOperation.printFilesInDirectory("C:\\AMD");
-        fileOperation.printAllInDirectory("C:\\AMD");
+        //fileOperation.printDirsInDirectory("C:\\AMD");
+        //fileOperation.printFilesInDirectory("C:\\AMD");
+        //fileOperation.printAllInDirectory("C:\\AMD");
+        System.out.println(fileOperation.isFilesEquals("C:\\kaka.txt", "C:\\AMD\\kaka.txt"));
     }
 }
